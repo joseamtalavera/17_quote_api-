b9e732a61ceb9e8186f83e1b4483771d1eb768b1
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-05-05 20:28:37 +0300

    change in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './public/script.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public/build'),
   },
   mode: process.env.NODE_ENV || 'development',
 };
